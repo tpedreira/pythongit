e18d34ec7a302139495c9646edbbcb615c4763d7
Tiago Pedreira <tiagopedreira@gmail.com>
2024-08-02 08:02:08 -0400

    Commit - moved files

--- project1/pythonProject/area.py
+++ project1/pythonProject/area.py
@@ -1,7 +1,7 @@
 length = int(input("What is the length of the rectangle? "))
 
 if length < 0:
-    print("Positive numbers only")
+    raise ValueError("Positive numbers only!")
     length = int(input("What is the length of the rectangle? "))
 
 
--- project1/pythonProject/week2-tuesday.py
+++ project1/pythonProject/week2-tuesday.py
@@ -1,1 +1,12 @@
 #trying to push
+
+
+length = int(input("What is the length?: "))
+
+width = int(input("What is the width?: "))
+
+height = float(input("What is the height? "))
+
+processing = length * width * height
+
+print("The overall output after processing is: ", processing)
