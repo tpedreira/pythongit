6fd2c326dc0f37f92ccf1294a9bedea66afb1d93
Tiago Pedreira <tiagopedreira@gmail.com>
2024-08-02 09:11:03 -0400

    Commit - moved files

---
+++ project1/pythonProject/assignment5.py
@@ -0,0 +1,9 @@
+# Time
+
+duration = float(input("Enter a time duration in hours: "))
+
+minutes = int(duration * 60)
+seconds = int(duration * 3600)
+
+print("The time in minutes is:", minutes, "and the time in seconds is:", seconds)
+
