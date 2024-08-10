40a6bbba1269a5f363beab778aae6135b244c3ea
Tiago Pedreira <tiagopedreira@gmail.com>
2024-08-10 11:21:52 -0400

    Commit

--- project1/pythonProject/assignment8-grades.py
+++ project1/pythonProject/assignment8-grades.py
@@ -6,13 +6,17 @@ mark3 = float(input("Enter your mark for subject 3: "))
 
 grade = (mark1 + mark2 + mark3) / 3
 
-if grade >= 90:
-    print("Your grade is an A")
-elif grade >= 80 and grade < 90:
-    print("Your grade is a B")
-elif grade >= 70 and grade < 80:
-    print("Your grade is a C")
-elif grade >= 60 and grade < 70:
-    print("Your grade is a D")
+if grade >= 0 and grade <= 100:
+    if grade >= 90:
+        print("Your grade is an A", round(grade, 2))
+    elif grade >= 80 and grade < 90:
+        print("Your grade is a B", round(grade, 2))
+    elif grade >= 70 and grade < 80:
+        print("Your grade is a C", round(grade, 2))
+    elif grade >= 60 and grade < 70:
+        print("Your grade is a D", round(grade, 2))
+    else:
+        print("Your grade is an F", round(grade, 2))
 else:
-    print("Your grade is an F")
+    print("Error: Please enter a only numbers between 0 and 100")
+
