6dd9ac72a7401fe836dde5e3248c7054f99470a7
Tiago Pedreira <tiagopedreira@gmail.com>
2024-08-10 09:50:18 -0400

    Commit

--- project1/pythonProject/assignment9-letter.py
+++ project1/pythonProject/assignment9-letter.py
@@ -1,24 +1,41 @@
 # Determine if a character is a vowel or consonant
 # This one took me a little bit to figure out how to get the program to go back to the Enter a letter but I finally got it!
 # Im actually proud to have done it all on my own. The only things I needed to research was the .isalpha method
-def letterCheck():
-    letter = str(input("Enter a letter to determine if it is a Vowel or Consonant: ")).upper()
-    if letter == "A":
-        print("The letter A is a vowel")
-    elif letter == "E":
-        print("The letter E is a vowel")
-    elif letter == "I":
-        print("The letter I is a vowel")
-    elif letter == "O":
-        print("The letter O is a vowel")
-    elif letter == "U":
-        print("The letter U is a vowel")
-    elif:
-        print("The letter " + letter + " is a consonant")
-    else: letter != letter.isalpha():
-        print("Please enter a letter")
-        letterCheck()
 
-letterCheck()
+# MY CODE BEFORE WATCHING VIDEO OF THURSDAYS CLASS
+# def letterCheck():
+#     letter = input("Enter a letter to determine if it is a Vowel or Consonant: ").upper()
+#     if letter == "A":
+#         print("The letter A is a vowel")
+#     elif letter == "E":
+#         print("The letter E is a vowel")
+#     elif letter == "I":
+#         print("The letter I is a vowel")
+#     elif letter == "O":
+#         print("The letter O is a vowel")
+#     elif letter == "U":
+#         print("The letter U is a vowel")
+#     elif:
+#         print("The letter " + letter + " is a consonant")
+#     else: letter != letter.isalpha():
+#         print("Please enter a letter")
+#         letterCheck()
+#
+# letterCheck()
 
 
+# CLASS CODE
+
+char_input = input("Enter a single character: ")
+
+if len(char_input) == 1 and char_input.isalpha():
+    char = char_input.lower()
+    if char in "aeiou":
+        print("The character is a vowel")
+    else:
+        print("The character is a consonant")
+else:
+    if len(char_input) != 1:
+        print("Error: Please enter only one character")
+    else:
+        print("Error: The input is not a letter")
