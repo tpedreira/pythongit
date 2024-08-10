13b5b3794d2d64fd7b83a050202a8b9721730949
Tiago Pedreira <tiagopedreira@gmail.com>
2024-08-10 10:41:11 -0400

    Commit

--- project1/pythonProject/assignment10-age.py
+++ project1/pythonProject/assignment10-age.py
@@ -1,10 +1,28 @@
 # Enter an age - Adult, Minor, Senior
 
-age = int(input("What is your age? "))
+# MY WORK BEFORE THURSDAYS CLASS
+# age = int(input("What is your age? "))
+#
+# if age < 18:
+#     print("You are a minor!")
+# elif age >= 18 and age <= 65:
+#     print("You are an Adult!")
+# elif age > 65:
+#     print("You are a Senior Citizen!")
 
-if age < 18:
-    print("You are a minor!")
-elif age >= 18 and age <= 65:
-    print("You are an Adult!")
-elif age > 65:
-    print("You are a Senior Citizen!")
+age_input = input("Enter your age: ")
+
+if age_input.isdigit():
+    age = int(age_input)
+
+    if age > 0:
+        if age < 18:
+            print("Minor")
+        elif 18 <= age <= 65:
+            print("Adult")
+        else:
+            print("Senior Citizen")
+    else:
+        print("Error: Age must be a positive number")
+else:
+    print("Error: Invalid input. Please enter a positive number")
