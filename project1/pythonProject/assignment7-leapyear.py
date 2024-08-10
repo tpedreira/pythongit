9b8b230b1d605602a9417a4e321628dac8543364
Tiago Pedreira <tiagopedreira@gmail.com>
2024-08-10 10:54:22 -0400

    Commit

--- project1/pythonProject/assignment7-leapyear.py
+++ project1/pythonProject/assignment7-leapyear.py
@@ -1,5 +1,6 @@
 # Determine if a year is a leap year
 
+# MY CODE BEFORE CLASS
 # year = int(input("Enter the year to determine if it is a leap year or not: "))
 #
 # def leapyear(year):
@@ -11,13 +12,28 @@
 #         return year, "is a leap year!"
 #     return False
 
-y = int(input("enter your year : "))
+# y = int(input("Enter your year : "))
+#
+# if (y % 400 == 0) or (y % 4 == 0 and y % 100 != 0) :
+#
+#     print("It is a leap year")
+#
+# else :
+#
+#     print("It is not a leap year")
 
-if (y % 400 == 0) or (y % 4 == 0 and y % 100 != 0) :
+# MY CODE AFTER CLASS
+y = input("Enter your year : ")
 
-    print("It is a leap year")
+if y.isdigit():
+    year = int(y)
 
-else :
+    if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0) :
 
-    print("It is not a leap year")
+        print("It is a leap year")
 
+    else :
+
+        print("It is not a leap year")
+else:
+    print("Error: Please enter a positive number for the year")
