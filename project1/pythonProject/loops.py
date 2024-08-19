7906a15eb5eb85ea6b4fa7d6c0bbce8616bced83
Tiago Pedreira <tiagopedreira@gmail.com>
2024-08-19 09:43:01 -0400

    Commit

---
+++ project1/pythonProject/loops.py
@@ -0,0 +1,22 @@
+word = "python"
+
+for letter in word:
+    print(letter)
+
+
+for x in range(5):
+    print(x)
+
+
+
+count = 1
+
+while count <= 5:
+    print("Count is: ", count)
+    count += 1
+
+number = 10
+
+while number > 0:
+    print("Number is: ", number)
+    number -= 2
