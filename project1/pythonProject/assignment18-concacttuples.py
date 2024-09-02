f82685fea50e8fbd51444bc3134fdf37e4846cd7
Tiago Pedreira <tiagopedreira@gmail.com>
2024-09-02 11:08:07 -0400

    Commit

---
+++ project1/pythonProject/assignment18-concacttuples.py
@@ -0,0 +1,11 @@
+
+
+def concat_tuples(tuple1, tuple2):
+    return tuple1 + tuple2
+
+tuplea = (1, 2, 3)
+tupleb = (4, 5, 6)
+
+results = concat_tuples(tuplea, tupleb)
+print("Concatenated Tuple: ", results)
+
